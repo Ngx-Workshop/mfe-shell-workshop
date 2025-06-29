58cd125f67bac4bd5d28fff50e0b4bedfad05212
Wesley DuSell <dusellfam@gmail.com>
2025-06-29 01:08:12 -0400

    Connecting the seed mfe remote

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'seed-mfe',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: '/remotes/ngx-seed-mfe/remoteEntry.js',
+        exposedModule: './Component',
+      }).then(m => m.App) // Adjust if your remote exposes a different module
+  }
+];
