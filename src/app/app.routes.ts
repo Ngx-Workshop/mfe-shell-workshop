31652fe62957069f3f61202c86395705fe0a681b
Wesley DuSell <dusellfam@gmail.com>
2025-08-10 02:20:48 -0400

    Testing Routing Paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
-import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'seed-mfe',
+    path: '',
     loadComponent: () =>
       loadRemoteModule({
         type: 'module',
         remoteEntry: '/remotes/ngx-seed-mfe/remoteEntry.js',
         exposedModule: './Component',
-      }).then(m => m.App) // Adjust if your remote exposes a different module
-  }
+      }).then((m) => m.App), // Adjust if your remote exposes a different module
+  },
 ];
