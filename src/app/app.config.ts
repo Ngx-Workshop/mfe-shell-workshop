483ed9e14b0bfec0e453b480151d7e9b7c53fea4
Wesley DuSell <dusellfam@gmail.com>
2025-08-10 02:18:53 -0400

    Abstracting icon register

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,55 +11,13 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
+import { registerIcons } from './app.icons';
 import { routes } from './app.routes';
 import { MfeRegistryService } from './services/mfe-registry.service';
 
 function initializerFn() {
   const mfeRegistryService = inject(MfeRegistryService);
   return firstValueFrom(mfeRegistryService.loadMfeRemotes());
-}
-
-function registerIcons(
-  matIconRegistry: MatIconRegistry,
-  domSanitizer: DomSanitizer
-) {
-  return () => {
-    const openAiIconSvg = document.querySelector(
-      '#openai_white_logomark'
-    )?.outerHTML;
-    openAiIconSvg &&
-      matIconRegistry.addSvgIconLiteral(
-        'openai_white_logomark',
-        domSanitizer.bypassSecurityTrustHtml(openAiIconSvg)
-      );
-
-    const rxjsIconSvg = document.querySelector(
-      '#rxjs_white_logomark'
-    )?.outerHTML;
-    rxjsIconSvg &&
-      matIconRegistry.addSvgIconLiteral(
-        'rxjs_white_logomark',
-        domSanitizer.bypassSecurityTrustHtml(rxjsIconSvg)
-      );
-
-    const nestjsIconSvg = document.querySelector(
-      '#nestjs_white_logomark'
-    )?.outerHTML;
-    nestjsIconSvg &&
-      matIconRegistry.addSvgIconLiteral(
-        'nestjs_white_logomark',
-        domSanitizer.bypassSecurityTrustHtml(nestjsIconSvg)
-      );
-
-    const angularIconSvg = document.querySelector(
-      '#angular_white_logomark'
-    )?.outerHTML;
-    angularIconSvg &&
-      matIconRegistry.addSvgIconLiteral(
-        'angular_white_logomark',
-        domSanitizer.bypassSecurityTrustHtml(angularIconSvg)
-      );
-  };
 }
 
 export const appConfig: ApplicationConfig = {
