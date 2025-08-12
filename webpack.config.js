060646ae307c791a2428a71b86ab013f728db154
Wesley DuSell <dusellfam@gmail.com>
2025-08-12 01:15:32 -0400

    shareAll is convenient but can bite you later.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,61 @@
 const {
   shareAll,
   withModuleFederationPlugin,
-} = require("@angular-architects/module-federation/webpack");
+} = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
   remotes: {
-    mfe1: "http://localhost:3000/remoteEntry.js",
+    mfe1: 'http://localhost:3000/remoteEntry.js',
   },
 
   shared: {
-    ...shareAll({
+    // ...shareAll({
+    //   singleton: true,
+    //   strictVersion: true,
+    //   requiredVersion: "auto",
+    // }),
+    '@angular/core': {
       singleton: true,
       strictVersion: true,
-      requiredVersion: "auto",
-    }),
+      requiredVersion: 'auto',
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/forms': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+
+    // If you use Material/CDK, share them too
+    '@angular/cdk': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/material': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+
+    // RxJS + tslib
+    rxjs: { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    tslib: { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+    // Only if you’re NOT zoneless:
+    'zone.js': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
   },
 });
